7666ef28ad5372f196754c5715685e6369bbd615
YOOJIYUN <ys7638@naver.com>
2021-07-04 00:59:10 +0900

    fix: overlap code

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,6 @@ class App {
     this.app.use(cors());
     this.app.use(express.urlencoded({ extended: false }));
     this.app.use(express.json());
-    this.app.use(cors({ origin: "*", credential: true }));
   }
   private setRouter(controller: Controller) {
     this.app.get("/", (req, res) => {
